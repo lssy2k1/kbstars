3760f557e75e40fdf35d50ecbd3d5e3e86267a60
lssy2k1 <lssy2k1@gmail.com>
2023-07-29 14:18:46 +0900

    first commit 1

---
+++ demo1/src/test/java/com/kbstars/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
